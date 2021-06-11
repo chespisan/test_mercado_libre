cf9bacb478297e95c6e1cc39f7e65ff150c7ba98
Sergio Sanchez <sergiosan2802@gmail.com>
2021-06-10 19:58:19 -0500

    feat: created structure init

---
+++ client/src/routes/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { hot } from 'react-hot-loader/root'
+import Home from '@containers/home/Home'
+
+const App = () => {
+
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+      </Switch>
+    </Router>
+  )
+
+}
+
+export default hot(App)
