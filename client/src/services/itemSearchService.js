bb716ac27911c9b5c9a77311f70ed8842255c023
Sergio Sanchez <sergiosan2802@gmail.com>
2021-06-12 18:14:27 -0500

    feat: branch only api

--- client/src/services/itemSearchService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const { REACT_APP_API_URL } = process.env
-
-export const getItems = async (query) => {
-  const { data } = await axios.get(`${REACT_APP_API_URL}items?q=${query}`)
-  return data
-}
--- client/src/utils/currencyFormat.js
+++
@@ -1,4 +0,0 @@
-export const currencyFormat = (num) => {
-  if (!num) return
-  return '$ ' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
-}
