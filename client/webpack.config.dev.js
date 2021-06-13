b4fb6932ee826d68e483df89eee06d7a5aa10a71
Sergio Sanchez <sergiosan2802@gmail.com>
2021-06-13 13:17:04 -0500

    feat: added config webpack performance

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
@@ -24,6 +24,11 @@ module.exports = {
       '@assets': path.resolve(__dirname, 'src/assets/'),
       '@utils': path.resolve(__dirname, 'src/utils/'),
     }
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   },
   module: {
     rules: [
@@ -62,10 +67,9 @@ module.exports = {
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    compress: true,
+    compress: false,
     historyApiFallback: true,
     port: 3005,
-    // hot: true
   },
   optimization: {
     splitChunks: {
